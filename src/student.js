14b6b276bed63c841f2eed85a01b6c4b0687e293
emrekaraca2 <69522407+emrekaraca2@users.noreply.github.com>
2020-09-03 21:00:18 +0200

    pair programming code

--- src/student.js
+++ src/student.js
@@ -4,21 +4,60 @@
 // sortDirection can be 'asc' or 'desc'
 // Make a shallow copy of the array before sorting it!
 const sortMembers = (team, sortBy, sortDirection) => {
-  return team;
+  const arrayCopy = [...team];
+  arrayCopy.sort((a, b) => {
+    var nameA = a.name.toUpperCase();
+    var nameB = b.name.toUpperCase();
+    var roleA = a.role.toUpperCase();
+    var roleB = b.role.toUpperCase();
+    if (sortBy === "name") {
+      if (nameA < nameB) {
+        return -1;
+      }
+      if (nameA > nameB) {
+        return 1;
+      }
+
+      // names must be equal
+      return 0;
+    } else {
+      if (roleA < roleB) {
+        return -1;
+      }
+      if (roleA > roleB) {
+        return 1;
+      }
+
+      // names must be equal
+      return 0;
+    }
+  });
+
+  return arrayCopy;
 };
 
 // Exercise 2
 const getStudents = (team) => {
-  // return all members who are students
-  return team;
+  return team.filter((teamMember) => {
+    return teamMember.role === "student";
+  });
 };
 const getTeachers = (team) => {
-  // return all members who are teachers
-  return team;
+  const filteredMembers = team.filter((teamMember) => {
+    return teamMember.role === "teacher";
+  });
+  return filteredMembers;
 };
 
 // Exercise 3
 const getMemberAnimalsWithUrls = (member) => {
+  return member.animals.map((animal) => {
+    return {
+      name: animal,
+      url: `https://www.randomlists.com/img/animals/${animal}.jpg`
+    };
+  });
+
   // return an array of objects where each object has the following shape:
   // { name: ANIMAL_NAME, url: ANIMAL_IMAGE_URL }
   // You can create the url by using this url and replacing filling in the animal name:
@@ -30,7 +69,14 @@ const getMemberAnimalsWithUrls = (member) => {
 const getAnimals = (team) => {
   // return an array of all animals that exist on all members
   // Do not include duplicate animals
-  return [];
+  return team.reduce((acc, member) => {
+    member.animals.forEach((animal) => {
+      if (acc.includes(animal) === false) {
+        acc.push(animal);
+      }
+    });
+    return acc;
+  }, []);
 };
 
 // Exercise 5
@@ -45,5 +91,5 @@ export {
   getTeachers,
   getMemberAnimalsWithUrls,
   getAnimals,
-  filterByAnimal,
+  filterByAnimal
 };
